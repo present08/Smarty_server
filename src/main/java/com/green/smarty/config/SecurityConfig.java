d154e6ef2c18ad6ea1c4970290958d912d85b1d4
Muam Jo <66456590+present08@users.noreply.github.com>
2025-01-14 12:47:42 +0900

    Update SecurityConfig.java

--- src/main/java/com/green/smarty/config/SecurityConfig.java
+++ src/main/java/com/green/smarty/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://43.201.209.78", "http://www.smartyacademy.shop"));
+        configuration.setAllowedOrigins(List.of("http://43.201.209.78", "http://smartyacademy.shop"));
         configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true); // 인증 정보를 포함한 요청 허용
