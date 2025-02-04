0746fb79d35b97951e49772288d43c4641a3b814
muamjo <present_08@naver.com>
2025-02-04 21:11:00 +0900

    nginx cors

--- src/main/java/com/green/smarty/config/SecurityConfig.java
+++ src/main/java/com/green/smarty/config/SecurityConfig.java
@@ -57,7 +57,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                List.of("http://43.201.209.78", "http://www.smartyacademy.shop", "http://localhost:3000"));
+                List.of("http://43.201.209.78", "http://www.smartyacademy.shop/green",
+                        "http://www.smartyacademy.shop/blue", "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true); // 인증 정보를 포함한 요청 허용
